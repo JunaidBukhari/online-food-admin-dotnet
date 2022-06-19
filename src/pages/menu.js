0238ec2798686bf5c56406e89d50a4dc53ad8fd8
Junaid Bukhari <imjunaid1997@gmail.com>
2022-06-19 15:49:59 +0500

    delete api integrated

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,39 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
-import AddEditFoodModal from './addEditFood';
-import { useSelector, useDispatch } from 'react-redux';
-import RatingStars from '../components/ratings';
-import { addToCart } from '../redux-toolkit/dataSlice';
-import { getMenu } from '../redux-toolkit/actions';
+import React, { useEffect, useState } from "react";
+import AddEditFoodModal from "./addEditFood";
+import { useSelector, useDispatch } from "react-redux";
+import RatingStars from "../components/ratings";
+import { DeleteFood, getMenu } from "../redux-toolkit/actions";
 const Menu = () => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.data.menu);
-  const cart = useSelector((state) => state.data.cart);
+ const handleDelete=(d)=>{
+  if (window.confirm("Are You Sure you want to Delete !")) {
+    dispatch(DeleteFood(d.id));
+  }
+ }
   useEffect(() => {
     dispatch(getMenu());
   }, []);
-  const additemtoCart = (d) => {
-    if (cart.filter((c) => c.id === d.id).length) {
-      let newCart = [...cart];
-      let obj = newCart.filter((c) => c.id === d.id)?.[0];
-      let number = obj.item;
-      let newObj = { ...obj, item: ++number };
-      let index = newCart.indexOf(obj);
-      newCart[index] = newObj;
-      toast.success(`you have now ${newObj.item} ${d.title}`);
-      dispatch(addToCart(newCart));
-    } else {
-      const data = [...cart, { ...d, item: 1 }];
-      dispatch(addToCart(data));
-      toast.success(`${d.title} is Added to cart`);
-    }
-  };
+ 
   const [obj, setObj] = useState({});
   const [show, setShow] = useState(false);
   return (
     <div className="container ">
       <div
-        style={{ marginBottom: '100px' }}
+        style={{ marginBottom: "100px" }}
         className="row d-flex justify-content-center"
       >
         <AddEditFoodModal
@@ -46,28 +33,28 @@ const Menu = () => {
           <div key={d.id} className="col-12 mt-4">
             <div
               style={{
-                maxHeight: '80px',
-                minHeight: '80px',
-                backgroundColor: 'white',
+                maxHeight: "80px",
+                minHeight: "80px",
+                backgroundColor: "white",
               }}
               className="card-body"
             >
               <span
                 className="d-flex"
-                style={{ justifyContent: 'space-between' }}
+                style={{ justifyContent: "space-between" }}
               >
                 <h5 className="col-4 text-dark">{d.name}</h5>
                 <img
                   style={{
-                    cursor: 'pointer',
-                    objectFit: 'cover',
+                    cursor: "pointer",
+                    objectFit: "cover",
                   }}
                   height={50}
                   width={50}
                   src={d.image}
                   alt=""
                 />
-                <h5 className="col-2 mt-2" style={{ color: 'green' }}>
+                <h5 className="col-2 mt-2" style={{ color: "green" }}>
                   Rs.{d.price}
                 </h5>
                 <div>
@@ -75,12 +62,12 @@ const Menu = () => {
                 </div>
               </span>
               <span
-                style={{ position: 'absolute', bottom: '10px', left: '40px' }}
+                style={{ position: "absolute", bottom: "10px", left: "40px" }}
               >
                 {<RatingStars rating={d.rating} />}
               </span>
               <button
-                style={{ position: 'absolute', bottom: '20px', right: '45px' }}
+                style={{ position: "absolute", bottom: "20px", right: "45px" }}
                 className="btn btn-success"
                 onClick={() => {
                   setObj(d);
@@ -90,9 +77,11 @@ const Menu = () => {
                 <i className="fa-solid fa-pen"></i>
               </button>
               <button
-                style={{ position: 'absolute', bottom: '20px', right: '95px' }}
+                style={{ position: "absolute", bottom: "20px", right: "95px" }}
                 className="btn btn-danger"
-                onClick={() => additemtoCart(d)}
+                onClick={() => {
+                  handleDelete(d);
+                }}
               >
                 <i className="fa-solid fa-trash"></i>
               </button>
@@ -100,9 +89,12 @@ const Menu = () => {
           </div>
         ))}
         <button
-          onClick={() => {setObj({});setShow(true)}}
+          onClick={() => {
+            setObj({});
+            setShow(true);
+          }}
           className="btn btn-success mt-5"
-          style={{ width: '150px' }}
+          style={{ width: "150px" }}
         >
           + Add Food
         </button>
--- src/redux-toolkit/actions.js
+++ src/redux-toolkit/actions.js
@@ -1,21 +1,35 @@
-import axios from 'axios';
-import { setMenu } from './dataSlice';
-import { serverUrl } from './../constants/serverRoutes';
-import { uploadImagetoCloudinary } from './../utils/imageUpload';
+import axios from "axios";
+import { setMenu } from "./dataSlice";
+import { serverUrl } from "./../constants/serverRoutes";
+import { uploadImagetoCloudinary } from "./../utils/imageUpload";
+import { toast } from "react-hot-toast";
 export const getMenu = () => (dispatch) => {
-  axios
-    .get(serverUrl)
-    .then((res) => dispatch(setMenu(res.data)));
+  axios.get(serverUrl).then((res) => dispatch(setMenu(res.data)));
 };
 
-export const AddandUpdate=(body,setShow)=>(dispatch)=>{
-  axios.post(serverUrl,body).then(res=>{setShow(false); dispatch(getMenu())})
-}
+export const AddandUpdate = (body, setShow) => (dispatch) => {
+  axios
+    .post(serverUrl, body)
+    .then((res) => {
+      setShow(false);
+      dispatch(getMenu());
+      toast.success("Success");
+    })
+    .catch((err) => {
+      toast.error("Error while saving");
+    });
+};
 
-export const AddandUpdateWithImage=(body,image,setShow)=>(dispatch)=>{
-  uploadImagetoCloudinary(image).then(result=>{
-    const newBody={...body, image:result.data.secure_url}
-    dispatch(AddandUpdate(newBody,setShow))
+export const AddandUpdateWithImage = (body, image, setShow) => (dispatch) => {
+  uploadImagetoCloudinary(image).then((result) => {
+    const newBody = { ...body, image: result.data.secure_url };
+    dispatch(AddandUpdate(newBody, setShow));
   });
- 
-}
+};
+
+export const DeleteFood = (id) => (dispatch) => {
+  axios.delete(`${serverUrl}/${id}`).then((res) => {
+    dispatch(getMenu());
+    toast.success("deleted successfully");
+  });
+};
