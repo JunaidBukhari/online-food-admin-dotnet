993dcc2d55574c4f2b99e7daf032f73927a98581
Junaid Bukhari <imjunaid1997@gmail.com>
2022-07-02 18:38:38 +0500

    admin orders

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,14 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getOrders } from "../redux-toolkit/actions";
-import { getUsers } from "./../redux-toolkit/actions";
+import { getUsers, getOrders, getMenu } from "../redux-toolkit/actions";
 const Orders = () => {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.data.orders);
+  const users = useSelector((state) => state.data.users);
+  const menu = useSelector((state) => state.data.menu);
   useEffect(() => {
     dispatch(getOrders());
     dispatch(getUsers());
+    dispatch(getMenu());
   }, []);
+
+  const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    setArray(
+      orders.map((o) => {
+        return {
+          OrderID: o.id,
+          amount: o.amount,
+          status: o.status,
+          paymentMethod: o.paymentMethod,
+          Food: requiredfood(o.foodId),
+          User: requireduser(o.userId),
+        };
+      })
+    );
+  }, [orders, users, menu]);
+
+  console.log(array);
+  const requiredfood = (id) => {
+    return menu.filter((m) => m.id === id)[0];
+  };
+  const requireduser = (id) => {
+    return users.filter((u) => u.id === id)[0];
+  };
   return (
     <div className="container">
       <div className="mt-5">
@@ -22,48 +49,46 @@ const Orders = () => {
               <td>Name of Customer</td>
               <td>Address</td>
               <td>Contact Number</td>
+              <td>Payment Method</td>
+              <td>Status</td>
               <td>Action</td>
             </tr>
           </thead>
           <tbody>
-            {orders.map((c) => (
+            {array.map((c) => (
               <tr>
-                <td>{c.id}</td>
-                <td>{c.title}</td>
+                <td>{c.OrderID}</td>
+                <td>{c.Food?.name}</td>
                 <td>
                   <span className="ml-3" style={{ color: "green" }}>
-                    {c.item}
+                    {c.amount}
                   </span>
                 </td>
 
-                <td>Rs {c.item * c.price}</td>
-                <td>Junaid</td>
-                <td>House # 120, Southern District Bahria Orchard Lahore</td>
-                <td>+923014254498</td>
+                <td>Rs {c.amount * c.Food?.price}</td>
+                <td>{c.User?.name}</td>
+                <td>{c.User?.address}</td>
+                <td>{c.User?.phone}</td>
+                <td>{c.paymentMethod}</td>
+                <td>{c.status}</td>
                 <td>
-                  <i
-                    className="fa fa-trash"
-                    style={{ color: "red", cursor: "pointer" }}
-                  ></i>
-                  <i
-                    onClick={() => {}}
-                    className="fa fa-pen ml-4"
-                    style={{ color: "blue", cursor: "pointer" }}
-                  ></i>
-                  <i
-                    onClick={() => {}}
-                    className="fa fa-check ml-4"
-                    style={{ color: "green", cursor: "pointer" }}
-                  ></i>
+                  <div className="d-flex">
+                    <i
+                      className="fa fa-trash"
+                      style={{ color: "red", cursor: "pointer" }}
+                    ></i>
+                    <i
+                      onClick={() => {}}
+                      className="fa fa-check ml-4"
+                      style={{ color: "green", cursor: "pointer" }}
+                    ></i>
+                  </div>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      {/* <button disabled={!cart.length} className='btn btn-success mt-3'>
-        {cart.length ? 'APPROVE ORDER' : 'NO ORDERS YET'}
-      </button> */}
     </div>
   );
 };
